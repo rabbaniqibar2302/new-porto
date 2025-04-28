b92dd7f5bf16ee52a9561b255c5fc2be936b369d
rabbaniqibar2302 <rizkiakbar2302@gmail.com>
2025-04-28 13:41:14 +0700

    done for headin

--- src/JS/gsap.js
+++ src/JS/gsap.js
@@ -2,33 +2,45 @@ import { gsap } from "gsap";
 
 // Setup
 const bgColor = "#FEFBF7";
-const easing = "none"; 
+const easing = "none";
 
 const tl = gsap.timeline({
   repeat: -1,
-  yoyo: true
+  yoyo: true,
+  duration: 2
 });
 
 // top
-tl.fromTo("#top-side", 
+tl.fromTo("#top-side",
   { width: 0, backgroundColor: bgColor, ease: easing },
-  { width: "100%" , backgroundColor: bgColor, duration: 2.5}
+  { width: "100%", backgroundColor: bgColor, duration: 2.5 }
 );
 
 // right
-tl.fromTo("#right-side", 
+tl.fromTo("#right-side",
   { height: 0, backgroundColor: bgColor, ease: easing },
   { height: "100%", backgroundColor: bgColor, duration: 1 }
 );
 
 // bottom
-tl.fromTo("#bottom-side", 
+tl.fromTo("#bottom-side",
   { width: 0, backgroundColor: bgColor, ease: easing },
   { width: "100%", backgroundColor: bgColor, duration: 2.5 }
 );
 
 // left
-tl.fromTo("#left-side", 
+tl.fromTo("#left-side",
   { height: 0, backgroundColor: bgColor, ease: easing },
   { height: "100%", backgroundColor: bgColor, duration: 1 }
+);
+
+// Text Animation
+const tl_text = gsap.timeline({
+  repeat: -1,
+  yoyo: true
+})
+
+tl_text.fromTo("#text_btm",
+  { y: -50, duration: 10, ease: "power1.inOut" },
+  { y: 42, duration: 10, ease: "power1.inOut" }
 );
